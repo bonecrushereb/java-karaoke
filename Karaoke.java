dc57b3ca214557b8f0386aa46e7bb3d5757a5798
bonecrushereb <bonecrushereb@gmail.com>
2016-07-07 11:52:28 -0700

    able to prompt user and add songs

--- Karaoke.java
+++ Karaoke.java
@@ -1,17 +1,12 @@
+import com.teamtreehouse.KaraokeMachine;
 import com.teamtreehouse.model.Song;
 import com.teamtreehouse.model.SongBook;
 
 public class Karaoke {
 
   public static void main(String[] args) {
-    Song song = new Song(
-      "The Beatles",
-      "Hey Jude",
-      "https://www.youtube.com/watch?v=A_MjCqQoLLA"
-    );
     SongBook songBook = new SongBook();
-    System.out.printf("Adding %s %n", song);
-    songBook.addSong(song);
-    System.out.printf("There are %d song(s). %n", songBook.getSongCount());
+    KaraokeMachine machine = new KaraokeMachine(songBook);
+    machine.run();
   }
 }
